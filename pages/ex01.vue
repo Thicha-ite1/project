2e487c7dcbea65dea02dd299907316c7fdd1d3c4
Thicha-ite1 <ita5932@ccollege.ac.th>
2020-07-08 15:57:01 +0700

    start

---
+++ pages/ex01.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Hello: {{ name }}</h2>
+    <h2>age: {{ age }}</h2>
+    <input type="text" :value="name">
+    <input type="text" v-model="name">
+    <div>
+    <button @click="doSave">SAVE</button>
+    </div>
+    <div>
+        <button @click="doMenu">MAIN MENU</button>
+    </div>
+    <div>
+        <button @click="doHome">HOME</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Thicha comnungpon",
+      age: 33,
+    }
+  },
+  methods:{
+    doSave(){
+    console.log("DO SAVE");
+    },
+    doMenu(){
+        this.$router.replace('menu')
+    },
+  },
+}
+</script>
